fa619378bc4f1231a739569a3e949b962a29c250
AS00557100 <40860333+AS00557100@users.noreply.github.com>
2018-07-05 18:29:12 +0530

    simple mvc

---
+++ SimpleMVC/SimpleMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using SimpleMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        MydbEntities1 db;
+        public EmployeeController()
+        {
+            db = new MydbEntities1();
+        }
+        // GET: Employee 
+        public ActionResult Index()
+        {
+            var employees = db.Employees;
+            return View(employees);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var employee = db.Employees.Where(e => e.Id == id).FirstOrDefault();
+
+            return View(employee);
+        }
+        public ActionResult Create()
+        {
+            Employee empToAdd = new Employee();
+
+            return View(empToAdd);
+        
+        }
+    }
+}
---
+++ SimpleMVC/SimpleMVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using SimpleMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //MydbEntities1 db = new MydbEntities1();
+            //var employees = db.Employees;
+            return View();
+        }
+    }
+}
